38e7dd08237e9208d485ee7beaf6ea5b728d3fe9
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-08 15:34:07 -0500

    Add search input

--- src/App/App.js
+++ src/App/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import "../apiCalls"
 import {useState, useEffect} from "react"
+import {Routes, Route} from "react-router-dom"
 import getWordDefinition from '../apiCalls';
 import SearchDefinitions from '../Components/SearchDefinitions/SearchDefinitions';
 import WordContainer from '../Components/WordContainer/WordContainer';
+
 
 
 function App() {
   
   const [selectedWord, setSelectedWord] = useState({}) // set state to nothing??
   const [networkError, setNetworkError] = useState("")
+  const [searchType, setSearchType] = useState("dictionary")
 
 
-const handleSearch = (searchedWord) => {
+const handleSearch = (searchedWord, searchType) => {
+  if(searchType === "dictionary")
+  setSearchType(searchType)
   getWordDefinition(searchedWord) 
   .then((word) => {
     setSelectedWord(word[0])
@@ -24,9 +29,11 @@ const handleSearch = (searchedWord) => {
 
   return (
     <main className="App">
-      <h1>Word Atlas</h1>
+        <h1>Word Atlas</h1>
+      {/* <Routes> */}
       <SearchDefinitions handleSearch={handleSearch}/>
       <WordContainer className="word-container" selectedWord={selectedWord}/>
+      {/* </Routes> */}
     </main>
   )
 }
