860d803df4cd572d49911187a970a19fe32e0130
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-07 20:19:00 -0500

    Add WIP WordContainer component

--- src/App/App.js
+++ src/App/App.js
@@ -2,31 +2,32 @@ import './App.css';
 import "../apiCalls"
 import {useState, useEffect} from "react"
 import getWordDefinition from '../apiCalls';
+import SearchDefinitions from '../Components/SearchDefinitions/SearchDefinitions';
+import WordContainer from '../Components/WordContainer/WordContainer';
+
 
 function App() {
 
-  const [word, setWord] = useState([])
+  const [selectedWord, setSelectedWord] = useState([])
   const [networkError, setNetworkError] = useState("")
+  const [searchedWord, setSearchedWord] = useState("")
 
   useEffect(() => {
-    getWordDefinition() // pass in searched word from from
+    getWordDefinition("slowly") // pass in searched word from from
     .then(word => {
-      console.log("word", word)
-      setWord(word)
+      // console.log("word in App:", word[0])
+      setSelectedWord(word[0])
     })
     .catch(error => setNetworkError(error.message))
   }, [])
 
-
-
-
-
   return (
     <main className="App">
       <h1>Word Atlas</h1>
-      
+      {/* <SearchDefinitions searchedWord={searchedWord} setSearchedWord={setSearchedWord}/> */}
+      <WordContainer selectedWord={selectedWord}/>
     </main>
-  );
+  )
 }
 
 export default App;
