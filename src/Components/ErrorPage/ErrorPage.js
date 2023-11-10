833d600facccba5649be41092149c88f4aee98c5
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-10 14:19:43 -0500

    Add styling to ErrorPage

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -1,20 +1,14 @@
 import "./ErrorPage.css"
 import {Link} from "react-router-dom"
 
-
 function ErrorPage({networkError, resetError}) {
-  console.log("inside ErrorPage:", networkError)
-
-//   const errorMessage = networkError&& networkError.message ? networkError.message : "Unknown error occurred."
-
   return (
       <div className="serverError">
-          <p>Oh no! {networkError}</p> 
+          <p>Oh no! {networkError}. Please try again.</p> 
           <Link to={"/"} onClick={() => {resetError()}} className="home-link">
               <button className="return-button">Return Home</button>
           </Link>
       </div>
   )
 }
-
 export default ErrorPage
