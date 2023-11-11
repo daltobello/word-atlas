a17ad2c5b198bece5cae0c8f71b5f3d293460a59
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-11 17:52:36 -0500

    Update ErrorPage to reset searched word

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -2,11 +2,11 @@ import "./ErrorPage.css"
 import {Link} from "react-router-dom"
 import PropTypes from 'prop-types';
 
-function ErrorPage({networkError, resetError, errorMessage}) {
+function ErrorPage({networkError, resetError, errorMessage, setSelectedWord}) {
   return (
       <div className="serverError">
          <p className="error-message">{errorMessage ? errorMessage : `Oh no! ${networkError}. Please try again.`}</p>
-          <Link to={"/"} onClick={() => {resetError()}} className="home-link">
+          <Link to={"/"} onClick={() => {resetError(); setSelectedWord();}} className="home-link">
               <button className="return-button">Return Home</button>
           </Link>
       </div>
