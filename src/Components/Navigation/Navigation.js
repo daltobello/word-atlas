d235625e0826f9c1dea669b5a3fee873413bd51a
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-15 09:44:30 -0500

    Refactor nav styling

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,10 +4,10 @@ import { Router, NavLink } from "react-router-dom";
 function Navigation() {
   return (
     <nav className="nav-wrapper">
-        <NavLink to={"/dictionary"} className="nav-link">
+        <NavLink to={"/dictionary"} className="nav-link" activeClassName="active-link">
           Dictionary
         </NavLink>
-        <NavLink to={"/thesaurus"} className="nav-link">
+        <NavLink to={"/thesaurus"} className="nav-link" activeClassName="active-link">
           Thesaurus
         </NavLink>
     </nav>
