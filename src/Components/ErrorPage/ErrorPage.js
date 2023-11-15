a2e79ebac6556eb897b0be507f84a8c6c0b6a115
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-15 13:25:57 -0500

    Add func to handle error reset

--- src/Components/ErrorPage/ErrorPage.js
+++ src/Components/ErrorPage/ErrorPage.js
@@ -1,13 +1,31 @@
 import "./ErrorPage.css"
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 import PropTypes from 'prop-types';
 
+
 function ErrorPage({networkError, resetError, errorMessage, setSelectedWord}) {
+
+  const navigate = useNavigate()
+
+  const handleError = () => {
+    if(!resetError && !setSelectedWord) {
+      // console.log(window.location.pathname)
+      navigate(-1)
+// if path name includes "thesaurus" nav to "thesaurus" else nav to "dict"
+// put slash after thesaurus. then split. 
+    } else {
+      resetError()
+      setSelectedWord()
+    }
+  }
+
+
+
   return (
       <div className="serverError">
          <p className="error-message">{errorMessage ? errorMessage : `Oh no! ${networkError}. Please try again.`}</p>
-          <Link to={"/"} onClick={() => {resetError(); setSelectedWord();}} className="home-link">
-              <button className="return-button">Return Home</button>
+          <Link to={"/"} onClick={() => {handleError()}} className="home-link">
+              <button className="return-button">Reset</button>
           </Link>
       </div>
   )
