e914505ef993ca1a1c0174c0394a37473bb3ae09
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-01-05 15:34:13 -0500

    Add toggleTheme fn via ThemeContext provider

---
+++ src/contexts/ThemeContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState } from "react"
+
+const ThemeContext = createContext()
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode)
+  }
+
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = () => {
+  return useContext(ThemeContext)
+}
