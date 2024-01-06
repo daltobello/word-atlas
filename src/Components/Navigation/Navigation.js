a034445b251ac62945f8acd3e36b94678e638a59
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2024-01-05 19:12:44 -0500

    Fix theme toggle

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import { Router, NavLink } from "react-router-dom";
 
 function Navigation() {
   return (
-    <nav className="nav-wrapper">
+    <nav className="nav-wrapper h-1/5 flex items-center justify-center">
         <NavLink to={"/"} className="nav-link" activeclassname="active-link">
           Dictionary
         </NavLink>
